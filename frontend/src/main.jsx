e77bcdd2693978764a8947286b9f152ffb43e1ce
Minnie-Mhie <aishaatinukeaisha@gmail.com>
2026-03-24 01:45:53 -0700

    fix main jsx for vercel deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,10 +7,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import "./App.css"
 import appSlice from "./redux/appSlice.js"
 import axios from "axios";
+import axiosInstance from "./api/Axios.js";
 
 // axios.get("http://localhost:5000/api/v1/products").catch(() => {})
 
-  fetch("/api/hello").catch(() => {})
+  axiosInstance.get("/v1/products").catch(() => {})
+
+  // fetch("/api/hello").catch(() => {})
 
   //axios.post("/api/v1/request-otp", data)
 
